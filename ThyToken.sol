080bf17247865b321a3d9ed1e20710f81f57f062
venturecheta <100167800+venturecheta@users.noreply.github.com>
2022-02-23 12:33:29 +0000

    Create ThyToken.sol

---
+++ ThyToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0; 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ThyToken is ERC20 {
+    constructor() ERC20('Thy Token', 'TTN'){
+        _mint(msg.sender, 10000 * 10**18);
+    }
+
+}
